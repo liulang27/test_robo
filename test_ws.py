5f47f9a2b7c334ab7eeac3685bf4bb9b1064b442
liulang27 <1320690090@qq.com>
2022-05-05 21:01:04 +0800

    three commit

--- test_ws.py
+++ test_ws.py
@@ -4,25 +4,27 @@ from selenium import webdriver
 
 class TestClass():
 
-    def setUp(self):
-        self.driver = webdriver.Chrome()
-        self.base_url = "http://www.testclass.net"
+    # def setUp(self):
+    #     self.driver = webdriver.Chrome()
+    #     self.base_url = "http://www.testclass.net"
 
-    def tearDown(self):
-        time.sleep(2)
-        self.driver.quit()
+    # def tearDown(self):
+    #     time.sleep(2)
+    #     self.driver.quit()
 
     def test_case(self):
-        self.driver.get(self.base_url)
-        search_input = self.driver.find_element_by_name("q")
-        search_input.send_keys("selenium")
-        search_input.submit()
+        # self.driver.get(self.base_url)
+        # search_input = self.driver.find_element_by_name("q")
+        # search_input.send_keys("selenium")
+        # search_input.submit()
+        print("hahahahahahahah")
 
     def test_case2(self):
-        self.driver.get(self.base_url)
-        search_input = self.driver.find_element_by_name("q")
-        search_input.send_keys("jenkins")
-        search_input.submit()
+        # self.driver.get(self.base_url)
+        # search_input = self.driver.find_element_by_name("q")
+        # search_input.send_keys("jenkins")
+        # search_input.submit()
+        print("ccccccccc")
 
 
 # if __name__ == '__main__':
